8fa61033343969ab94540d103f52e33a34dad330
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-06-11 10:28:12 +0530

    script.js

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ let submit=document.getElementById("submit");
 
 let bar=document.getElementById("bar");
 cvBtn.addEventListener("click",function(){
-    alert("Download succesfully");
+    alert("Please Click OK");
 })
 
 
-submit.addEventListener("click",function(){
-    let email=document.getElementById("email");
-    let pass=document.getElementById("pass");
+// submit.addEventListener("click",function(){
+//     let email=document.getElementById("email");
+//     let pass=document.getElementById("pass");
 
-    if(email.value == "" && pass.value == ""){
-        alert("Filled details");
-    }
-    else{
-        alert("Logged in")
-    }
-})
+//     if(email.value == "" && pass.value == ""){
+//         alert("Filled details");
+//     }
+//     else{
+//         alert("Logged in")
+//     }
+// })
 
 let ul=document.querySelector("ul");
 bar.addEventListener("click",function(){
